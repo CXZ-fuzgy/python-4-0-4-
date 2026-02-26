29acb659a0c52e202b9939b88ae0acebb883021a
陈小洲 <2047381445@qq.com>
2026-02-26 19:26:34 +0800

    Add files via upload

---
+++ python day1.py
@@ -0,0 +1,25 @@
+name = input("你的名字：")
+age  = input("你的年龄：")
+city = input("你的位置：") 
+
+print("\n ============== ")
+print("我是",name)
+print("年龄",age)
+print("位置",city)
+print("\n ==============")
+
+A1 = int(input("数字一："))              #加法数学计算
+A2 = int(input("数字二："))
+
+#计算输出
+A3 = A1 + A2
+print(A3)
+
+# 摄氏度转华氏度：F = C × 1.8 + 32
+C = float(input("请输入摄氏度"))
+F = C * 1.8 + 32
+print(F"华氏度")
+
+print("简易计算器")
+print("1.加  2.减  3.乘  4.除")
+
